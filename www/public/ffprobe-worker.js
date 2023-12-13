2aba715eaeb6593c69d29c73473c3f392bc6b574
Christian Sonne <cers@geeksbynature.dk>
2023-12-13 16:03:32 +0100

    ensure we still clean up if ffprobe fails

--- www/public/ffprobe-worker.js
+++ www/public/ffprobe-worker.js
@@ -10,41 +10,45 @@ onmessage = (message) => {
     }
     FS.mount(WORKERFS, { files: [file] }, '/work')
 
-    // Call the wasm module.
-    const { name, ...info } = Module.get_file_info(`/work/${file.name}`)
+    try {
+      // Call the wasm module.
+      const { name, ...info } = Module.get_file_info(`/work/${file.name}`)
 
-    // Remap streams into collection.
-    const streams = []
-    for (let i = 0; i < info.streams.size(); i++) {
-      const tags = {}
-      for (let j = 0; j < info.streams.get(i).tags.size(); j++) {
-        const t = info.streams.get(i).tags.get(j)
-        tags[t.key] = t.value
+      // Remap streams into collection.
+      const streams = []
+      for (let i = 0; i < info.streams.size(); i++) {
+        const tags = {}
+        for (let j = 0; j < info.streams.get(i).tags.size(); j++) {
+          const t = info.streams.get(i).tags.get(j)
+          tags[t.key] = t.value
+        }
+        streams.push({ ...info.streams.get(i), tags })
       }
-      streams.push({ ...info.streams.get(i), tags })
-    }
 
-    // Remap chapters into collection.
-    const chapters = []
-    for (let i = 0; i < info.chapters.size(); i++) {
-      const tags = {}
-      for (let j = 0; j < info.chapters.get(i).tags.size(); j++) {
-        const t = info.chapters.get(i).tags.get(j)
-        tags[t.key] = t.value
+      // Remap chapters into collection.
+      const chapters = []
+      for (let i = 0; i < info.chapters.size(); i++) {
+        const tags = {}
+        for (let j = 0; j < info.chapters.get(i).tags.size(); j++) {
+          const t = info.chapters.get(i).tags.get(j)
+          tags[t.key] = t.value
+        }
+        chapters.push({ ...info.chapters.get(i), ...{ tags } })
       }
-      chapters.push({ ...info.chapters.get(i), ...{ tags } })
+
+      // Send back data response.
+
+      origin.postMessage({
+        streams,
+        chapters,
+        format: {
+          ...info,
+          format_name: name,
+        },
+      })
+    } catch (error) {
+      origin.postMessage({ error })
     }
-
-    // Send back data response.
-
-    origin.postMessage({
-      streams,
-      chapters,
-      format: {
-        ...info,
-        format_name: name,
-      },
-    })
 
     // Cleanup mount.
     FS.unmount('/work')
